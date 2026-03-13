00a533f4911c9a02cbaefc614468607e5a2cffaf
CeanAlagarCGA <alagar.cf@obmontessori.edu.ph>
2026-03-13 23:37:56 +0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,20 @@
+function getTotal() {
+
+    const ticketPrice = 200;
+    const adminFee = 50;
+
+    let quantity = document.getElementById("price").value;
+
+    let subtotal = quantity * ticketPrice;
+    let total = subtotal + adminFee;
+
+    document.getElementById("subtotal").value = subtotal;
+    document.getElementById("total").value = total;
+}
+
+function completeRegistration() {
+
+    let name = document.getElementById("full_name").value;
+
+    alert("Registration Complete! Thank you, " + name);
+}
